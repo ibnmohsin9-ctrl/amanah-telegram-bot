d5420b97d291c69429871b6127b48053f72f14b0
ibnmohsin9-ctrl <ibnmohsin9@gmail.com>
2026-07-02 08:23:23 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 def main():
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN is not set!")
+
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot is running...")
+
+    import asyncio
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
     app.run_polling()
 
